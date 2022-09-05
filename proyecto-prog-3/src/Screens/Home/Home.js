7806d1e2bed02b7825fed62af3907d05d4d8f183
martumoroni <80982224+martumoroni@users.noreply.github.com>
2022-09-05 12:27:13 -0300

    agregue carpetas

---
+++ proyecto-prog-3/src/Screens/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Cards from "../../Component/Cards/Cards"
+
+/*export default class home extends Component {
+  constructor(props) {
+    super(props)
+    this.state=(
+    topList: {},
+    albums: {}
+  )
+}
+
+componentDidMount(){
+    fetch("")
+
+}
+ 
+}
+*/
