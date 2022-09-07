856d0c878e01fa750a9f41300217b0a2e2e90913
tomasdetrapaga123 <tdetrapaga@udesa.edu.ar>
2022-09-07 14:36:05 -0300

    footer terminado

--- proyecto-prog-3/src/Components/Footer/Footer.js
+++ proyecto-prog-3/src/Components/Footer/Footer.js
@@ -5,15 +5,16 @@ function Footer() {
     return (
         
         <footer className="footer">  
+        
+            <div><img className='digitahouselogo' src='../img/digitalhouselogo.jpg' alt="digitalhouse"/></div>
             <div>
-            <img className='digitahouselogo' src='../img/digitalhouselogo.png' alt="digitalhouse"/>
             <ul className="nombresequipo">
                     <li>Tomás de Trápaga</li>
                     <li>Franco Ferrero</li>
                     <li>Martina Moroni</li>
             </ul>
-            <img className='udesalogo' src='../img/udesalogo.png' alt="udesalogo"/>
             </div>
+            <div><img className='udesalogo' src='../img/udesalogo.png' alt="udesalogo"/></div>            
         </footer>
 
     );
