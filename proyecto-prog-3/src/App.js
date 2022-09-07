21575ac95ce04f4c2b19b11bf0879208b0ccf285
francoferrero22 <ferrerof@udesa.edu.ar>
2022-09-07 13:54:08 -0300

    footer añadido

--- proyecto-prog-3/src/App.js
+++ proyecto-prog-3/src/App.js
@@ -5,27 +5,16 @@ import Main from './Components/Main/Main'
 //import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 
 
-import PeliculaCard from './Components/PeliculaCard/PeliculaCard';
-import SerieCard from './Components/SerieCard/SerieCard';
+/*import PeliculaCard from './Components/PeliculaCard/PeliculaCard';
+import SerieCard from './Components/SerieCard/SerieCard'*/
 
 function App() {
-/* return(
-  <React.Fragment>
-    <Header/>
-<Main/>
-<Routes>
-<Route path = '/' exact = {true} component={Main}/>
-</Routes>
-
-
-  </React.Fragment>
-) */
-
 return(
   <React.Fragment>
 
     <Header/>
     <Main/>
+    <Footer/>
   </React.Fragment>
 )
 
