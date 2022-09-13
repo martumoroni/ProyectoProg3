a8423a2e3db22fc45aa093dea8b5c8865c9663d8
tomasdetrapaga123 <tdetrapaga@udesa.edu.ar>
2022-09-13 14:51:25 -0300

    termine el header

--- proyecto-prog-3/src/Components/Header/Header.js
+++ proyecto-prog-3/src/Components/Header/Header.js
@@ -4,18 +4,18 @@ import {Link} from 'react-router-dom';
 
 function Header(props) {
     
-    return (    
-        <header className="header">  
-        <div className='maineimagen' ><img className='imovieslogo' src='../img/imovies.jpeg' alt="imovie"/>
+    return (
+        <header className="header">
+            <div className='maineimagen'>
+                <img className='imovieslogo' src='../img/logodos.png' alt="imovie" />
                 <Link to='/'>Main</Link>
-        </div>
-            <div>
-            <ul className="headerlista">
-            <li><Link to='/favoritos'>Favoritos</Link></li>
-            <li> <Link to='/peliculas'>Peliculas</Link></li>
-            <li><Link to='/ver-todos'>Ver todos</Link></li>
-            </ul>
             </div>
+            <div className="headerlista">
+                <Link to='/favoritos'>Favoritos</Link>
+                <Link to='/peliculas'>Peliculas</Link>
+                <Link to='/ver-todos'>Ver todos</Link>
+            </div>
+
         </header>
 
     );
