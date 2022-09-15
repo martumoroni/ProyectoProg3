401eef3f83df3a883685651546f791b9ba0b0258
tomasdetrapaga123 <tdetrapaga@udesa.edu.ar>
2022-09-15 17:30:35 -0300

    detalle

--- proyecto-prog-3/src/Screens/DetallePelicula/DetallePelicula.js
+++ proyecto-prog-3/src/Screens/DetallePelicula/DetallePelicula.js
@@ -1,54 +1,99 @@
-import React, { Component } from "react";
-import Buscador from '../../Components/Buscador/Buscador'
-import CardDetallePelicula from '../../Components/CardDetallePelicula/CardDetallePelicula'
+/* c037da6882c3b5641c3425b1e80847bb */ 
 
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './DetallePelicula.css'
 
 class DetallePelicula extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            data: '',
-            id: Number(props.match.params.id)
+            props: props,
+            id: Number(props.match.params.id),
+            datosPelicula: [],
+            genres: '',
+            companies: {},
+            country: {},
+            mensaje: 'Agregar a favoritos',
         }
     }
 
     componentDidMount() {
-     /*  const url = window.location.href
-       console.log(url, 'la url padre')
-       const lastSegment = url.split("/").pop();
-console.log(lastSegment, 'barro') */
-
-        //Mejores Peliculas
         fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=c037da6882c3b5641c3425b1e80847bb`)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ data: data })
-                console.log(data, 'la informacion', this.state.data)
-            }
-                
-    
-            )
-            .catch(error => console.log('el error fue ' + error))
+            .then(res => res.json())
+            .then(data => this.setState({
+                datosPelicula: data,
+                genres: data.genres[0].name,
+                companies: data.production_companies[0],
+                country: data.production_countries[0]
+            }))
+            .catch(err => console.log(err))
 
+        let favoritos = [];
+        let recuperoStorage = localStorage.getItem('favoritos');
+
+        if (recuperoStorage !== null) {
+            favoritos = JSON.parse(recuperoStorage);
+        };
+
+        if (favoritos.includes(this.state.id)) {
+            this.setState({
+                mensaje: 'Quitar de favoritos'
+            });
+        };
+    }
+
+    modificarFavoritos(id){
+        let favoritos = [];
+        let recuperoStorage = localStorage.getItem('favoritos');
+
+        if(recuperoStorage !== null){
+            let favoritosToArray = JSON.parse(recuperoStorage);
+            favoritos = favoritosToArray;
         }
-    render(){
-        return(
+        if(favoritos.includes(id)){
+            let sacarFav = favoritos.indexOf(id);
+            favoritos.splice(sacarFav, 1);
+            this.setState({
+                mensaje: 'Agregar a favoritos'
+            })
+        } else {
+            favoritos.push(id);
+            this.setState({
+                mensaje: 'Quitar de favoritos'
+            })
+        }
 
-            <React.Fragment> 
-                <Buscador/>
+        let favoritosToString = JSON.stringify(favoritos);
+        localStorage.setItem('favoritos', favoritosToString);
+
+        console.log(localStorage)
+    }
+
+
+    render() {
+        return (
+            
+            <article className='detail-card'>
+
+                <img src={`https://image.tmdb.org/t/p/w500/${this.state.datosPelicula.poster_path}`} alt="" />
                 <div>
-                {this.state.data ?
-                <div>
-                <CardDetallePelicula titulo={this.state.data.title} image={this.state.data.poster_path}/>
-                
-
-                </div> : <div> Cargando...</div>}
-                {console.log('renderizado!', this.state.data)}
-
+                    <h2 className="title-detail">{this.state.datosPelicula.title}</h2>
+                    <p>{this.state.datosPelicula.overview}</p>
+                    <p>Fecha de estreno (AA-MM-DD): {this.state.datosPelicula.release_date}</p>
+                    <p>Califiación: {Math.round(this.state.datosPelicula.vote_average * 100) / 100}</p>
+                    <p>Duración: {this.state.datosPelicula.runtime} minutos</p>
+                    <p>Generos: {this.state.genres}</p>
+                    <p>Producción: {this.state.companies.name}</p>
+                    <p>País de producción: {this.state.country.name}</p>
+                    <p className="boton" onClick={()=>this.modificarFavoritos(this.state.id)}>{this.state.mensaje}</p>
                 </div>
-            </React.Fragment>
+
+            </article>
+
         )
     }
+    
 }
-export default DetallePelicula ;
 
+export default DetallePelicula;
