60f0062fd65fd4c57cd0d743ce329d65822bfe01
martumoroni <80982224+martumoroni@users.noreply.github.com>
2022-09-15 22:06:32 -0300

    modificac. para q se agreguen paginas hacia abajo

--- proyecto-prog-3/src/Components/VerTodasPeli/VerTodasPeli.js
+++ proyecto-prog-3/src/Components/VerTodasPeli/VerTodasPeli.js
@@ -1,35 +1,38 @@
 import React, {Component} from "react";
 import PeliculaCard from '../PeliculaCard/PeliculaCard';
+import SerieCard from "../SerieCard/SerieCard"
 
 
 class VerTodasPeli extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            contador: 1,
+            contadorPeli: 1,
             dataPeliculas: []
         }
     }
 
     componentDidMount() {
 
-        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=af93cf6a36d0e3597028097290f9535d")
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=af93cf6a36d0e3597028097290f9535d&language=en-US&page=${this.state.contadorPeli}`)
             .then(response => response.json())
             .then(data => this.setState(
                 { dataPeliculas: data.results,
-                  contador: this.state.contador + 1
+                  contador: this.state.contadorPeli + 1
                 }
             ))
             .catch(error => console.log('el error fue ' + error))
     }
 
-    buscarMas() {
+    buscarMasPeliculas() {
         
-        fetch("https://api.themoviedb.org/3/tv/popular?api_key=af93cf6a36d0e3597028097290f9535d&page=" + this.state.contador)
+        let peliculasViejas = this.state.dataPeliculas
+        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=af93cf6a36d0e3597028097290f9535d&page=${this.state.contadorPeli}`)
+
             .then(response => response.json())
             .then(data => this.setState(
-                { dataPeliculas: data.results,
-                  contador: this.state.contador + 1
+                { dataPeliculas: peliculasViejas.concat(data.results),
+                  contadorPeli: this.state.contadorPeli + 1
                 }
             ))
             .catch(error => console.log('el error fue ' + error))
@@ -41,11 +44,13 @@ class VerTodasPeli extends Component {
 
                 <div className="titulo-container">
                     <h2 className="Titulo">Todas las películas</h2>
-                    <button onClick={() => this.buscarMas()}></button>
+                    
                 </div>
                 <section className='card-container'>
-                    {this.state.dataPeliculas.map((unPelicula, idx )=> <PeliculaCard key={unPelicula + idx} data={unPelicula}  image={unPelicula.poster_path} title={unPelicula.name}/>)}
+                    {this.state.dataPeliculas.map((unPelicula, idx )=> <PeliculaCard key={unPelicula + idx} data={unPelicula}  image={unPelicula.poster_path} title={unPelicula.title}/>)}
                 </section>
+
+                <button onClick={() => this.buscarMasPeliculas()}>Buscar más películas</button>
 
             </React.Fragment>
 
