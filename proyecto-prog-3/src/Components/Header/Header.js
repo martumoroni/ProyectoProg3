4a16bb02f542525c5be5d268c596895849901496
martumoroni <80982224+martumoroni@users.noreply.github.com>
2022-09-17 18:12:49 -0300

    css footer y header

--- proyecto-prog-3/src/Components/Header/Header.js
+++ proyecto-prog-3/src/Components/Header/Header.js
@@ -8,15 +8,15 @@ function Header(props) {
         <header className="header">
         <div className='maineimagen'>
                 <img className='imovieslogo' src='../img/logodos.png' alt="imovie" />
-                <Link to='/'>Main</Link>
+                <Link className='headerTitulo' to='/'>IMOVIES</Link>
 
         </div>
                 <div>
-            <ul className="headerlista">
-            <li><Link to='/favoritos'>Favoritos</Link></li>
-            <li> <Link to='/peliculas'>Peliculas</Link></li>
-            <li><Link to='/Series'>Series</Link></li>
-            <li><Link to='/ver-todas'>Ver todos</Link></li>
+            <ul className='headerLista'>
+            <li><Link className='headerlista' to='/favoritos'>Favoritos</Link></li>
+            <li><Link className='headerlista' to='/peliculas'>Peliculas</Link></li>
+            <li><Link className='headerlista' to='/Series'>Series</Link></li>
+            <li><Link className='headerlista' to='/ver-todas'>Ver todos</Link></li>
             </ul>
                 </div>
 
